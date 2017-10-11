8a0b5a74d557be9d3832494febcde0a8d746d235
djulzz <julien.esposito@gmail.com>
2017-10-11 14:10:21 -0700

    Going Over Repear While
    Going Over Repear While

--- Swift/week2_practice.swift
+++ Swift/week2_practice.swift
@@ -8,3 +8,14 @@ while( increment < 11 )
 	print( "my increment is \(increment)")
 	increment = increment + 1
 }
+
+print( "\n\n\nnow using repeat while\n\n\n")
+
+// the repeat while loop is equivalent to 
+// a do-while in (Java, C#, C++)
+increment = 0
+repeat 
+{
+	print( "my increment is \(increment)")
+	increment = increment + 1
+} while( increment < 11 )
