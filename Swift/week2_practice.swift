fc7a294611cbc41e7a978cd3516870284e5eee32
djulzz <julien.esposito@gmail.com>
2017-10-11 13:05:40 -0700

    Introduction to while loops
    Introduction to while loops

--- Swift/week2_practice.swift
+++ Swift/week2_practice.swift
@@ -1,19 +1,10 @@
-// For loops in swift
-let gravity = 9.81
-var myVariable:Int?
 
-// First Version of for loops
-// using variables for start and stopping point
+// On the topic of while loop
 
-// var start = 1
-// var stop = 10
-// for i in start..<stop {
-// 	print( "Displaying i = \(i)" )
-// }
+var increment:Int = 0
 
-// for loop used to print every even number
-// within the range [0 10]
-
-for i in stride(from: 0, to: 10, by: 2) {
-    print("Current Even Number \(i)" )
+while( increment < 11 )
+{
+	print( "my increment is \(increment)")
+	increment = increment + 1
 }
