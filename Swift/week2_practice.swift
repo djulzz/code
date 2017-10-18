7e5abc63f45758f14a784849ae7e9f5ef8842022
djulzz <julien.esposito@gmail.com>
2017-10-17 17:03:41 -0700

    Finishing up week 2 and Adding Parrot Framework

--- Swift/week2_practice.swift
+++ Swift/week2_practice.swift
@@ -4,7 +4,19 @@ func CalculateGrossIncome( hourlyRate:Double, NumHoursWorked:Double ) -> Double
 	return Amount 
 }
 
+func FibonacciSequence( Rank:Int ) -> Int
+{
+	var term:Int = 1;
+	while()
+	{
+
+	}
+	let result = term;
+	return result;
+}
 
 var salary:Double = CalculateGrossIncome( hourlyRate:37.5, NumHoursWorked:40 )
+var term50:Int = FibonacciSequence( Rank:50 );
 
 print( "You gross income based hourly rate is $\(salary)")
+print( "The Fib number at position 50 is = \(term50 )")
---
+++ Swift/week3_mambo.swift
@@ -0,0 +1,19 @@
+
+func takeOff() -> Void
+{
+	print( "Taking Off...")
+}
+
+func wait(_ seconds:Int) -> Void
+{
+	print( "Waiting \(seconds) seconds")
+}
+
+func land() -> Void
+{
+	print( "Landing...")
+}
+
+takeOff()
+wait(4)
+land()
